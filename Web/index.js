aa6ec26e34ee7873033d1d95eba2cc405bd5abcc
AshCauseYes <ashton5018@gmail.com>
2024-12-03 12:18:44 -0330

    Start of Loan Analysis section. General structure of table and functioning buttons

--- Web/index.js
+++ Web/index.js
@@ -3,27 +3,62 @@
 // Dates:
 
 var $ = function (id) {
-  return document.getElementById(id);
+	return document.getElementById(id);
 };
 
 // Define format options for printing.
 const cur2Format = new Intl.NumberFormat("en-CA", {
-  style: "currency",
-  currency: "CAD",
-  minimumFractionDigits: "2",
-  maximumFractionDigits: "2",
+	style: "currency",
+	currency: "CAD",
+	minimumFractionDigits: "2",
+	maximumFractionDigits: "2",
 });
 
 const per2Format = new Intl.NumberFormat("en-CA", {
-  style: "percent",
-  minimumFractionDigits: "2",
-  maximumFractionDigits: "2",
+	style: "percent",
+	minimumFractionDigits: "2",
+	maximumFractionDigits: "2",
 });
 
 const com2Format = new Intl.NumberFormat("en-CA", {
-  style: "decimal",
-  minimumFractionDigits: "2",
-  maximumFractionDigits: "2",
+	style: "decimal",
+	minimumFractionDigits: "2",
+	maximumFractionDigits: "2",
 });
 
 // Start function definitions here.
+
+function LoanAnalysis() {
+	// Template
+	document.getElementById("loan").innerHTML = `
+    <p>Loan Analysis Statement</p>
+		<br />
+		<p>10 year options on $#,###.##</p>
+		<p>Reason: Xxxxxxxx Xxxxxxxxx Xxxxxxxxxx</p>
+		<p>Statement date: YYYY-MM-DD</p>
+		<br />
+		<p>Years&nbsp;&nbsp;Interest&nbsp;&nbsp;Total Amt&nbsp;&nbsp;Mon Payment</p>
+		<p>---------------------------------------</p>
+		<p>&nbsp;##&nbsp;&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;&nbsp;$#,###.##</p>
+		<p>&nbsp;##&nbsp;&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;&nbsp;$#,###.##</p>
+		<p>&nbsp;##&nbsp;&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;&nbsp;$#,###.##</p>
+		<p>&nbsp;##&nbsp;&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;&nbsp;$#,###.##</p>
+		<p>&nbsp;##&nbsp;&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;&nbsp;$#,###.##</p>
+		<p>&nbsp;##&nbsp;&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;&nbsp;$#,###.##</p>
+		<p>&nbsp;##&nbsp;&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;&nbsp;$#,###.##</p>
+		<p>&nbsp;##&nbsp;&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;&nbsp;$#,###.##</p>
+		<p>&nbsp;##&nbsp;&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;&nbsp;$#,###.##</p>
+		<p>&nbsp;##&nbsp;&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;$#,###.##&nbsp;&nbsp;&nbsp;$#,###.##</p>
+		<p>---------------------------------------</p>
+		<p>Payback option selected:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;## Years</p>
+    <button onclick="LoanPrep()">Clear</button>
+  `;
+	return;
+}
+
+function LoanPrep() {
+	document.getElementById("loan").innerHTML = `
+  <button onclick="LoanAnalysis()">Loan Analysis</button>
+  `;
+	return;
+}
